251f0117a667f928381b04a153c0455f636138b5
goater <goater1095@gmail.com>
2020-10-15 23:07:58 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -1,82 +1,85 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const port = 3000
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const port = 3000;
 
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb://localhost/todo-list", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 // 取得資料庫連線狀態
-const db = mongoose.connection
+const db = mongoose.connection;
 // 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+db.on("error", () => {
+  console.log("mongodb error!");
+});
 // 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
 
-const exhbs = require('express-handlebars')
-app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
+const exhbs = require("express-handlebars");
+app.engine("hbs", exhbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
 
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: true }))
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
+const Todo = require("./models/todo");
 
-const Todo = require('./models/todo')
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   Todo.find()
     .lean()
-    .then(todos => res.render('index', { todos }))
-    .catch(error => console.error(error))
-})
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
+    .sort({ _id: "asc" })
+    .then((todos) => res.render("index", { todos }))
+    .catch((error) => console.error(error));
+});
+app.get("/todos/new", (req, res) => {
+  return res.render("new");
+});
 
-app.post('/todos', (req, res) => {
-  const name = req.body.name
+app.post("/todos", (req, res) => {
+  const name = req.body.name;
   return Todo.create({ name })
-    .then(() => res.redirect('/'))
-    .catch(error => console.error(error))
-})
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
+    .then(() => res.redirect("/"))
+    .catch((error) => console.error(error));
+});
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id;
   return Todo.findById(id)
     .lean()
-    .then(todo => res.render('detail', { todo }))
-    .catch(error => console.error(error))
-})
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
+    .then((todo) => res.render("detail", { todo }))
+    .catch((error) => console.error(error));
+});
+app.get("/todos/:id/edit", (req, res) => {
+  const id = req.params.id;
   return Todo.findById(id)
     .lean()
-    .then(todo => res.render('edit', { todo }))
-    .catch(error => console.error(error))
-})
+    .then((todo) => res.render("edit", { todo }))
+    .catch((error) => console.error(error));
+});
 
-app.post('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
+app.post("/todos/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const { name, isDone } = req.body;
   return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      todo.save()
+    .then((todo) => {
+      todo.name = name;
+      todo.isDone = isDone === "on";
+      todo.save();
     })
     .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.error(error))
-})
+    .catch((error) => console.error(error));
+});
 
-app.post('/todos/:id/delete', (req, res) => {
-  const id = req.params.id
+app.post("/todos/:id/delete", (req, res) => {
+  const id = req.params.id;
   return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.error(error))
-})
+    .then((todo) => todo.remove())
+    .then(() => res.redirect("/"))
+    .catch((error) => console.error(error));
+});
 
 app.listen(port, () => {
-  console.log(`The Server is start on http://localhost:${port}`)
-})
+  console.log(`The Server is start on http://localhost:${port}`);
+});
