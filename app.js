dc9d3f2537eaafe198cd1abd6858b4f9f548d848
goater <goater1095@gmail.com>
2020-10-16 15:17:57 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const app = express();
 const port = 3000;
 const exhbs = require("express-handlebars");
-// const Todo = require("./models/todo");
 const bodyParser = require("body-parser");
 
 // express定義會自動尋找目錄的index的檔案
 const routes = require('./routes')
 
-mongoose.connect("mongodb://localhost/todo-list", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-// 取得資料庫連線狀態
-const db = mongoose.connection;
-// 連線異常
-db.on("error", () => {
-  console.log("mongodb error!");
-});
-// 連線成功
-db.once("open", () => {
-  console.log("mongodb connected!");
-});
-
+require('./config/mongoose')
 //set template engine
 app.engine("hbs", exhbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,15 +1,6 @@
-const mongoose = require('mongoose')
 const Todo = require('../todo')
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
+const db = require('../../config/mongoose')
 db.once('open', () => {
-  console.log('mongodb connected!')
   for (let i = 0; i < 10; i++) {
     Todo.create({ name: `name-${i}` })
   }
