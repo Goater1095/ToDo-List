a662a799a34640f26f50568881d651867abbe2a3
goater <goater1095@gmail.com>
2020-11-09 16:29:22 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-const express = require("express");
-const methodOverride = require("method-override");
+const express = require('express');
+const session = require('express-session');
+const methodOverride = require('method-override');
 const app = express();
 const port = process.env.PORT || 3000;
-const exhbs = require("express-handlebars");
-const bodyParser = require("body-parser");
+const exhbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 // express定義會自動尋找目錄的index的檔案
-const routes = require('./routes')
+const routes = require('./routes');
 
-require('./config/mongoose')
+require('./config/mongoose');
 //set template engine
-app.engine("hbs", exhbs({ defaultLayout: "main", extname: ".hbs" }));
-app.set("view engine", "hbs");
+app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', 'hbs');
 
 //set use
+app.use(
+  session({
+    secret: 'ThisIsMySecret',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(routes)
+app.use(methodOverride('_method'));
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`The Server is start on http://localhost:${port}`);
