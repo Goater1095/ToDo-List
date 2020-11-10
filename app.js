332e353e04878e890dbc9d321b82f0bc7e45eb67
goater <goater1095@gmail.com>
2020-11-11 02:03:46 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 // 載入設定檔，要寫在 express-session 以後
 const usePassport = require('./config/passport');
-
+const flash = require('connect-flash');
 const methodOverride = require('method-override');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -30,10 +30,13 @@ app.use(methodOverride('_method'));
 
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app);
+app.use(flash());
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.user = req.user;
+  res.locals.success_msg = req.flash('success_msg'); // 設定 success_msg
+  res.locals.warning_msg = req.flash('warning_msg'); // 設定 warning_msg
   next();
 });
 
