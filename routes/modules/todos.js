57b54db49fb220b0cdcbb186a7b3c6066b716cbe
goater <goater1095@gmail.com>
2020-11-11 00:07:35 +0800

    feat: add relation between User and Todo models

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -1,57 +1,60 @@
 //引用Express & Express 路由器
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 // 引用Todo Model
-const Todo = require('../../models/todo')
+const Todo = require('../../models/todo');
 // 定義路由
 
-router.get("/new", (req, res) => {
-  return res.render("new");
+router.get('/new', (req, res) => {
+  return res.render('new');
 });
 
-
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
+  const userId = req.user._id;
   const name = req.body.name;
-  return Todo.create({ name })
-    .then(() => res.redirect("/"))
+  return Todo.create({ name, userId })
+    .then(() => res.redirect('/'))
     .catch((error) => console.error(error));
 });
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  return Todo.findById(id)
+router.get('/:id', (req, res) => {
+  const userId = req.user._id;
+  const _id = req.params.id;
+  return Todo.findOne({ _id, userId })
     .lean()
-    .then((todo) => res.render("detail", { todo }))
+    .then((todo) => res.render('detail', { todo }))
     .catch((error) => console.error(error));
 });
-router.get("/:id/edit", (req, res) => {
-  const id = req.params.id;
-  return Todo.findById(id)
+router.get('/:id/edit', (req, res) => {
+  const userId = req.user._id;
+  const _id = req.params.id;
+  return Todo.findOne({ _id, userId })
     .lean()
-    .then((todo) => res.render("edit", { todo }))
+    .then((todo) => res.render('edit', { todo }))
     .catch((error) => console.error(error));
 });
 
-router.put("/:id", (req, res) => {
-  const id = req.params.id;
+router.put('/:id', (req, res) => {
+  const userId = req.user._id;
+  const _id = req.params.id;
   const { name, isDone } = req.body;
-  return Todo.findById(id)
+  return Todo.findOne({ _id, userId })
     .then((todo) => {
       todo.name = name;
-      todo.isDone = isDone === "on";
+      todo.isDone = isDone === 'on';
       todo.save();
     })
-    .then(() => res.redirect(`/todos/${id}`))
+    .then(() => res.redirect(`/todos/${_id}`))
     .catch((error) => console.error(error));
 });
 
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  return Todo.findById(id)
+router.delete('/:id', (req, res) => {
+  const userId = req.user._id;
+  const _id = req.params.id;
+  return Todo.findOne({ _id, userId })
     .then((todo) => todo.remove())
-    .then(() => res.redirect("/"))
+    .then(() => res.redirect('/'))
     .catch((error) => console.error(error));
 });
-
 
 //匯出路由模組
-module.exports = router
+module.exports = router;
